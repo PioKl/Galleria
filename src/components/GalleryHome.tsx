628fe29a71f4234b2851cb6223933729cde8e56d
PioKl <kosiu342@gmail.com>
2024-07-25 16:12:14 +0200

    Add name and artist to gallery image

--- src/components/GalleryHome.tsx
+++ src/components/GalleryHome.tsx
@@ -9,6 +9,13 @@ export default function GalleryHome() {
     <div className="galleryHome wrapper">
       {gallery.map((item, id) => (
         <a href="#" className="galleryHome__item" key={id}>
+          <div className="galleryHome__quick-info">
+            <div className="galleryHome__name-and-artist-container">
+              <h3 className="galleryHome__name">{item.name}</h3>
+              <h4 className="galleryHome__artist">{item.artist.name}</h4>
+            </div>
+          </div>
+
           <picture className="galleryHome__picture">
             <source media="(min-width: 1160px)" srcSet={item.images.gallery} />
             <source media="(min-width: 768px)" srcSet={item.images.gallery} />
