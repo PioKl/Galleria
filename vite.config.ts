c21f82eea8a7349fe7e6d422d068344493faf21c
PioKl <kosiu342@gmail.com>
2024-07-26 23:24:28 +0200

    Add base and env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Gallery/",
   plugins: [react()],
-})
+  define: {
+    "process.env": {
+      VITE_BASE_URL: "/Gallery/",
+    },
+  },
+});
