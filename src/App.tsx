9a4346ee0eff19c57636db760623a4c43526eff7
PioKl <kosiu342@gmail.com>
2024-07-29 20:44:48 +0200

    Add router and new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import data from "./data/data.json";
+import { Gallery } from "./types/types";
 import "./style/App.scss";
 import Header from "./components/Header";
 import GalleryHome from "./components/GalleryHome";
+import SingleGallerySlide from "./components/SingleGallerySlide";
+import ErrorPage from "./components/ErrorPage";
 
 function App() {
+  const gallery: Gallery[] = data;
   return (
-    <div>
+    <Router basename={process.env.VITE_BASE_URL}>
       <Header />
-      <GalleryHome />
-    </div>
+      <main className="main">
+        <Routes>
+          <Route path="*" element={<ErrorPage />} />
+          <Route path="/" element={<GalleryHome gallery={gallery} />} />
+          <Route
+            path="/slide/:slug"
+            element={<SingleGallerySlide gallery={gallery} />}
+          />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
