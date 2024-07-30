24c1910ed72fcc85a309b0b90d2d66e8e97c1ba5
PioKl <kosiu342@gmail.com>
2024-07-30 22:07:20 +0200

    Add Slider

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,46 @@
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "../style/Header.scss";
 import logo from "../assets/images/logo.svg";
-export default function Header() {
+
+interface HeaderProps {
+  startSlider: boolean;
+  setStartSlider: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Header: React.FC<HeaderProps> = ({ startSlider, setStartSlider }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  function handleStartSlideShow() {
+    if (location.pathname === "/") {
+      navigate(`/slide/0`);
+    }
+    setStartSlider(!startSlider);
+  }
   return (
     <header className="header">
-      <nav className="nav wrapper">
-        <img
-          className="nav__image"
-          src={logo}
-          alt="logo"
-          width={113}
-          height={32}
-        />
+      <nav className="nav">
+        <Link
+          to="/"
+          onClick={() => {
+            setStartSlider(false);
+          }}
+        >
+          <img
+            className="nav__image"
+            src={logo}
+            alt="logo"
+            width={113}
+            height={32}
+          />
+        </Link>
 
-        <a className="nav__link linkSlide" href="#">
-          Start Slideshow
-        </a>
+        <button onClick={handleStartSlideShow} className="nav__link link">
+          {!startSlider ? "Start slideshow" : "Stop slideshow"}
+        </button>
       </nav>
     </header>
   );
-}
+};
+
+export default Header;
