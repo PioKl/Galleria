1871b60c4425d26e07bcbb7a7b133be0523db9f6
PioKl <kosiu342@gmail.com>
2024-08-01 17:31:43 +0200

    Functionality update (counter, redirection, ref for sliderButton)

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,9 +1,37 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import "../style/ErrorPage.scss";
 
-export default function ErrorPage() {
+interface ErrorPageProps {
+  sliderButtonRef: React.RefObject<HTMLButtonElement>;
+}
+
+const ErrorPage: React.FC<ErrorPageProps> = ({ sliderButtonRef }) => {
+  const navigate = useNavigate();
+  const [counter, setCounter] = useState(5);
+  useEffect(() => {
+    sliderButtonRef.current && (sliderButtonRef.current.disabled = true);
+
+    const timer = setInterval(() => {
+      setCounter(counter - 1);
+    }, 1000);
+
+    counter === 0 && navigate("/");
+
+    return () => clearInterval(timer);
+  }, [counter, navigate, sliderButtonRef]);
   return (
-    <div>
-      <Link to="/">Go To Main Page</Link>
+    <div className="error-container">
+      <h2>Wrong Page</h2>
+      <span>
+        Return to{" "}
+        <Link to="/" className="errorLink">
+          main page{" "}
+        </Link>
+        in {counter}
+      </span>
     </div>
   );
-}
+};
+
+export default ErrorPage;
